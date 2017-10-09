a25426e33916418b934f53c10d457127a01ea1b8
Luan van Pletsen <luanvanpletsen@msn.com>
2017-10-09 16:06:24 +0100

    initilaization passed tests

--- note-model-spec.js
+++ note-model-spec.js
@@ -1,4 +1,9 @@
 (function() {
   var note = new Note("hello");
-  assert.isTrue(note.text() === "hello");
+  assert.isTrue(note.getText() === "hello");
+})();
+
+(function() {
+  var note = new Note("Luan VP");
+  assert.isTrue(note.getText() === "Luan VP");
 })();
--- note-model.js
+++ note-model.js
@@ -1,3 +1,9 @@
-function Note() {
-  
+function Note(text) {
+
+  this.text = text;
+
 }
+
+Note.prototype.getText = function() {
+  return this.text;
+};

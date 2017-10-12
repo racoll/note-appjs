1d00bb4400281d8f59976d02ca06b465af04b742
keith(mac) <ka33145@googlemail.com>
2017-10-12 12:48:43 +0100

    Add controller test

---
+++ spec/note-controller-spec.js
@@ -0,0 +1,20 @@
+
+
+(function(exports) {
+
+  function testNoteController() {
+
+    //Create something to test
+    myNoteController = new NoteController('Favourite drink: seltzer');
+    
+    myNoteController.renderText();
+
+    var appContent = document.getElementById('app').innerHTML; //Content of div with id = app
+
+    var expected = '<ul><li><div>Favourite drink: seltzer</div></li></ul>';
+
+    assert.isTrue(appContent === expected);
+}
+
+  testNoteController();
+})(this);

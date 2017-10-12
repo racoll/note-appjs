e42cdd1249ab93f6c0a26e42c9c96e695ceae89a
robincollins <racollins1@me.com>
2017-10-12 10:51:10 +0100

    reorganised index files and file structure

---
+++ src/note-model.js
@@ -0,0 +1,23 @@
+(function(exports) {
+
+  function Note(text) { // constructor function
+    this.text = text;
+  }
+
+  Note.prototype.getText = function() { //prototype function
+    return this.text;
+  };
+
+  exports.Note = Note;  //does the nme of exports mtter? wht rae you ctully doing here\??
+
+
+
+})(this);
+
+// what is a statement - // statements are executed one by one in the same order they are writen, separated by semi colons.
+// In HTML, statements are instructions to be executed by the web browser
+// what is a function - // a block of code designed to perform a particular task. It is executed when something invokes (calls) it.
+// what is an annonoymous function - //  a no-named function that is stored in a variable. They are always invoked using the variable name.
+// what is an immediately invoked function - // IIFE - to obtain data privacy. any variables declared within the IIFE cannot be accessed by the outside.
+
+// what is 'this' how does it change and why - 
